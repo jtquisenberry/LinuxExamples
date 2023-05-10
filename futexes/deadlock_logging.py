3112eec2f3164d47f5a3910d706079b092da12de
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2023-05-09 21:58:47 -0500

    Update deadlock_logging.py

--- futexes/deadlock_logging.py
+++ futexes/deadlock_logging.py
@@ -1,17 +1,37 @@
 from multiprocessing import Process
+import multiprocessing
 from threading import Thread
 from time import sleep
+import time
+import datetime
 import os
+import sys
 import logging
 
 def print_thread(ppid):
     placeholder = 1 
+    end = start = datetime.datetime.now() 
     i = 0
     while i < 2000:
     #while True:
         #placeholder += 1
         print("PRINT THREAD OF MAIN PROCESS PID:", ppid, flush=True)
         i += 1
+    diff = (end - start).total_seconds()
+    while diff < 1:
+        # print(diff)
+        end = datetime.datetime.now()
+        diff = (end - start).total_seconds()
+        sleep(0.5)
+    children = multiprocessing.active_children()
+    print(children)
+    if children:
+        print("CHILD PROCESS IS DEADLOCKED")
+        for child in children:
+            child.kill()
+        sys.exit(0)
+        sys.exit(0)
+        os._exit(0)
 
 def print_process():
     #sleep(120)
